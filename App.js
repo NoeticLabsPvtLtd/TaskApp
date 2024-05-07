ebcf9bd004bedd54dc49ae5c9f4b1c269ceedcb6
NoeticLabsPvtLtd <sominath.thore@noetic-labs.com>
2024-05-07 10:13:21 +0530

    added Navigation

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import NavigationComponant from './Componant/NavigationComponant.'
+
+export default function App() {
+  return (
+    <NavigationComponant />
+  )
+}
---
+++ Componant/NavigationComponant..js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../Screens/HomeScreen';
+import LoginScreen from '../Screens/LoginScreen';
+
+export default function NavigationComponant() {
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
